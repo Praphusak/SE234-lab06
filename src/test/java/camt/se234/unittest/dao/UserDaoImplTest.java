79cd2bfa14f6ae8c4c9225fd3958d04f9e399712
Apiwat Hantrakool <apiwat_hantrakool@cmu.ac.th>
2017-03-27 11:12:23 +0700

    Add all promblem1 test case

--- src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
+++ src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
@@ -1,13 +1,16 @@
 package camt.se234.unittest.dao;
 import camt.se234.unittest.entity.User;
+import camt.se234.unittest.exception.OldDateException;
 import camt.se234.unittest.service.UserServiceImpl;
-
+import org.hamcrest.Matcher;
+import org.hamcrest.core.Is;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
 import java.time.LocalDate;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 
@@ -17,9 +20,13 @@ public class UserDaoImplTest {
         UserDaoImpl userDao = new UserDaoImpl();
         assertThat(userDao.getUsers(),
                 hasItems(new User("Prayuth","1234","Tu",
-                                LocalDate.of(2016,12,28),"08612345678"),
+                                LocalDate.of(1979,2,14),"08612345678"),
                         new User("Tucky","5675","Tuckung",
-                                LocalDate.of(2014,11,22),"08687654321")
+                                LocalDate.of(1999,8,30),"08687654321"),
+                        new User("Honey","aabbcc","Honey",
+                                LocalDate.of(2012,11,13),"0000000000"),
+                        new User("None","none","NoName",
+                                LocalDate.of(2112,1,1),"9999999999")
                 ));
     }
 
@@ -34,7 +41,42 @@ public class UserDaoImplTest {
         userService.setUserDao(userDao);
         // check for the exception we expect
         thrown.expect(NullPointerException.class);
-        userService.login("","");
+        assertThat(userService.login("",""),is(java.util.Optional.of(null)));
+
+    }
+
+
+    @Test
+    public void testLogin(){
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao =new UserDaoImpl();
+        userService.setUserDao(userDao);
+        assertThat(userService.login("Prayuth","1234"),is(new User("Prayuth","1234","Tu",
+                LocalDate.of(1979,2,14),"08612345678")));
+    }
+
+    @Test
+    public void testIsAbleGoToPub(){
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao =new UserDaoImpl();
+        userService.setUserDao(userDao);
+        assertThat(userService.isAbleToGoToPub(new User("Prayuth","1234","Tu",
+                LocalDate.of(1979,2,14),"08612345678"),LocalDate.now()),is(true));
+
+        assertThat(userService.isAbleToGoToPub(new User("Honey","aabbcc","Honey",
+                LocalDate.of(2012,11,13),"0000000000"),LocalDate.now()),is(false));
+    }
+
+    @Test
+    public void testGetPubAllowanceUser(){
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao =new UserDaoImpl();
+        userService.setUserDao(userDao);
+        assertThat(userService.getPubAllowanceUser(LocalDate.now()),containsInAnyOrder(new User("Prayuth","1234","Tu",
+                LocalDate.of(1979,2,14),"08612345678")));
+
+       /* assertThat(userService.getPubAllowanceUser(LocalDate.now()),containsInAnyOrder());*/  //if anyone lessthan 20
+
     }
 
 
